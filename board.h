2ef81bc62052aa4a532d92fb54cbad649b5888f0
poesgamer <moosmegens@gmail.com>
2023-10-14 22:52:11 +0200

    Added important board variables
    (castling rights, whos move it is, where you can play enpassant and the ply counter + some WIP on the ThreeFoldRepetition)

--- board.h
+++ board.h
@@ -3,9 +3,47 @@
 
 #include <iostream>
 #include <cstdint>
+#include <vector>
 
 #include "enum.hpp"
 #include "move.h"
+
+
+class Board {
+public:
+
+    // Constructor
+    Board();
+
+
+    bool whiteOO  =true;
+    bool whiteOOO =true;
+
+    bool blackOO  =true;
+    bool blackOOO =true;
+
+    uint64_t enpassant;
+
+    int plyCount = 0;
+
+    //std::vector<uint64_t bitboards[12]> threeFoldRepetition;
+
+
+    // Really important
+    bool isWhite = true;
+
+    uint64_t pieces[12]; // One for each piece type
+
+   
+    void setPiece(Piece piece, Square square);
+
+    void MakeMove(Move move, int piece);
+
+    void PrintBoard(uint64_t bitboards[12]);
+    
+};
+
+
 
 // Bitboards for files A, B, G, and H
     extern uint64_t FILE_A;
@@ -19,23 +57,5 @@
 // Bitboards for ranks 2 and 7
     extern uint64_t secondRankBitboard;
     extern uint64_t seventhRankBitboard;
-
-class Board {
-public:
-    uint64_t pieces[12]; // One for each piece type
-
-
-
-
-    Board();
-    
-
-    void setPiece(Piece piece, Square square);
-
-    void MakeMove(Move move, int piece);
-
-    void PrintBoard(uint64_t bitboards[12]);
-    
-};
 
 #endif
