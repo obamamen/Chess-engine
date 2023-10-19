ad52674943dd4aefa270471e35594fe93b29f10e
poesgamer <moosmegens@gmail.com>
2023-10-19 13:26:37 +0200

    ..

--- board.h
+++ board.h
@@ -58,4 +58,13 @@ public:
     extern uint64_t secondRankBitboard;
     extern uint64_t seventhRankBitboard;
 
+//constexpr U64 north(U64 b) { return b << 8; }
+//constexpr U64 south(U64 b) { return b >> 8; }
+//constexpr U64 west(U64 b) { return (b & ~FILE_A) << 1; }
+//constexpr U64 east(U64 b) { return (b & ~FILE_H) >> 1; }
+//constexpr U64 north_west(U64 b) { return (b & ~FILE_A) << 9; }
+///constexpr U64 north_east(U64 b) { return (b & ~FILE_H) << 7; }
+//constexpr U64 south_west(U64 b) { return (b & ~FILE_A) >> 7; }
+//constexpr U64 south_east(U64 b) { return (b & ~FILE_H) >> 9; }
+
 #endif
